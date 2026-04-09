9e09050621a4a2be8c3e44c094befd78e07decfc
celtichero2026 <celtichero2026@gmail.com>
2026-04-09 13:42:56 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD_ID = 1414539813473878141  # your server ID
+
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -11,10 +11,10 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     guild = discord.Object(id=GUILD_ID)
-    await bot.tree.sync(guild=guild)
+  await bot.tree.sync()
     print(f"Logged in as {bot.user}")
 
-@bot.tree.command(name="bid", description="Post a bid", guild=discord.Object(id=GUILD_ID))
+@bot.tree.command(name="bid", description="Post a bid")
 async def bid(
     interaction: discord.Interaction,
     toon: str,
